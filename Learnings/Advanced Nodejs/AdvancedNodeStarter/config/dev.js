d8b2fd918ea09b3e4ad1eb27767c11816ade092a
tanveer-98 <tnvrahmed98@gmail.com>
2022-09-05 08:41:51 +0530

    updated

--- Learnings/Advanced Nodejs/AdvancedNodeStarter/config/dev.js
+++ Learnings/Advanced Nodejs/AdvancedNodeStarter/config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: "mongodb://tnvrahmed98:#Riku1998@ac-qhfpu82-shard-00-00.f6dhcgb.mongodb.net:27017,ac-qhfpu82-shard-00-01.f6dhcgb.mongodb.net:27017,ac-qhfpu82-shard-00-02.f6dhcgb.mongodb.net:27017/blog-dev?ssl=true&replicaSet=atlas-vghshx-shard-0&authSource=admin&retryWrites=true&w=majority",
   cookieKey: '123123123',
 };
